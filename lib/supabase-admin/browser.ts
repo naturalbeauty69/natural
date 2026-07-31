4520b0671ecb24b811d34ecb29bcda73fe491e7c
krish555-png <krigrg555@gmail.com>
2026-07-31 12:45:41 +0545

    update files

--- lib/supabase-admin/browser.ts
+++ lib/supabase-admin/browser.ts
@@ -1,13 +1,8 @@
 import { createBrowserClient } from "@supabase/ssr";
 
-console.log("SUPABASE URL =", process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log(
-  "SUPABASE KEY EXISTS =",
-  !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
-
-export const createClient = () =>
-  createBrowserClient(
+export function createClient() {
+  return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
+}
